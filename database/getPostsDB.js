25197dc9d1ad3601e7f446ae3bf0fc181a7d72db
Tony Ng <ngkokhong.tony@gmail.com>
2020-09-26 14:53:19 +0800

    added prettier lib

--- database/getPostsDB.js
+++ database/getPostsDB.js
@@ -1,18 +1,15 @@
-
 // define the home page route
 const getAllPostsByThreadIdDB = (req) => {
   const statement = `SELECT * from post WHERE thread_id = ?`;
-  return new Promise(function(resolve, reject){
+  return new Promise(function (resolve, reject) {
     db.query(statement, [req.params.threadId], (err, result) => {
       if (err) {
-        console.log(err)
-        reject(new Error('get ALL POSTS ERROR'))
+        console.log(err);
+        reject(new Error("get ALL POSTS ERROR"));
       }
-      resolve(result)
-    })
-  })
-}
+      resolve(result);
+    });
+  });
+};
 
-export { 
-  getAllPostsByThreadIdDB
-}
+export { getAllPostsByThreadIdDB };
